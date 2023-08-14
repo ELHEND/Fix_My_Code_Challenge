2cf7a88b8b611d743195d9f5aa57a0ad6a259e14
ELHEND <alhnd683@gmail.com>
2023-08-14 09:16:47 -0700

    saw

---
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+
+/**
+ *  * add_dnodeint_end - Add  node at end of a list
+ *   *
+ *    * @head:  address of  pointer to the first element of the list
+ *     * @n: number to store in the new element
+ *      *
+ *       * Return:  pointer to  new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *l;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	l = *head;
+	while (l->next != NULL)
+	{
+		l = l->next;
+	}
+	l->next = new;
+	new->prev = l;
+	return (new);
+}
+

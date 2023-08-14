84febc6fd71843b7d3caaab6376b4c4aa52ecd2c
ELHEND <alhnd683@gmail.com>
2023-08-14 09:07:26 -0700

    aaa

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
-    Print a square with the character #
+    Print  square with  character #
     
-    The size of the square must be the first argument 
+    size of the square must be the first argument 
     of the program.
 */
 
@@ -16,9 +16,10 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
+for (let m = 0 ; m < size ; m ++) {
     for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
 }
+
